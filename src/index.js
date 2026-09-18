293d83a62a10a9e9e26d30527ae9dd7c110d3dc0
udbhav07 <udbhavsai.k@gmail.com>
2026-09-19 03:11:15 +0530

    Add short-link redirects on r.udbhavsai.com

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * This worker redirects my short and memorable links to my pages.
  */
+const Links = {
+	'/linkedin': 'https://www.linkedin.com/in/udbhav-sai-kukkadapu-a36a93287/',
+	'/github': 'https://github.com/udbhav07',
+	'/portfolio': 'https://udbhavsai.com',
+	'': 'https://udbhavsai.com',
+};
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+		const url = new URL(request.url);
+		const path = url.pathname.toLowerCase().replace(/\/+$/, '');
+		const target = Links[path] ?? Links[''];
+		return Response.redirect(target, 302);
 	},
 };
